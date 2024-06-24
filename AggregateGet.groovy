1821a6400695f81affe31a0a4f3088664618d1bd
kyusoo <rbtn110@uengine.org>
2024-06-24 13:56:52 +0900

    groovy파일 수정

--- AggregateGet.groovy
+++ AggregateGet.groovy
@@ -66,43 +66,41 @@ org.springframework.cloud.contract.spec.Contract.make {
 }
 
 
-<function>
-    window.$HandleBars.registerHelper('checkOutgoing', function (relation) {
-        for(var i = 0; i < relation.length; i++){
-            if(!relation[i].target._type.includes('Command') || !relation[i].target.examples){
-                return true;
-            }
+window.$HandleBars.registerHelper('checkOutgoing', function (relation) {
+    for(var i = 0; i < relation.length; i++){
+        if(!relation[i].target._type.includes('Command') || !relation[i].target.examples){
+            return true;
         }
-        return false;
-    })
+    }
+    return false;
+})
 
-    window.$HandleBars.registerHelper('setExampleType', function (key, value, aggregateList, aggregate) {
-        var type = 'String'
-        if(aggregateList){
-            for(var i = 0; i < aggregateList.length; i++){
-                for(var j = 0; j< aggregateList[i].aggregateRoot.fieldDescriptors.length; j++){
-                    if(aggregateList[i].aggregateRoot.fieldDescriptors[j].name == key){
-                        type = aggregateList[i].aggregateRoot.fieldDescriptors[j].className
-                    }
-                }
-                
-            }
-        }else if(!aggregateList && aggregate){
-            for(var i = 0; i < aggregate.aggregateRoot.fieldDescriptors.length; i++){
-                if(aggregate.aggregateRoot.fieldDescriptors[i].name == key){
-                    type = aggregate.aggregateRoot.fieldDescriptors[i].className
+window.$HandleBars.registerHelper('setExampleType', function (key, value, aggregateList, aggregate) {
+    var type = 'String'
+    if(aggregateList){
+        for(var i = 0; i < aggregateList.length; i++){
+            for(var j = 0; j< aggregateList[i].aggregateRoot.fieldDescriptors.length; j++){
+                if(aggregateList[i].aggregateRoot.fieldDescriptors[j].name == key){
+                    type = aggregateList[i].aggregateRoot.fieldDescriptors[j].className
                 }
             }
+            
         }
-        
-        return type;
-    })
+    }else if(!aggregateList && aggregate){
+        for(var i = 0; i < aggregate.aggregateRoot.fieldDescriptors.length; i++){
+            if(aggregate.aggregateRoot.fieldDescriptors[i].name == key){
+                type = aggregate.aggregateRoot.fieldDescriptors[i].className
+            }
+        }
+    }
+    
+    return type;
+})
 
-    window.$HandleBars.registerHelper('checkExtendVerbType', function (type, path) {
-        if(type == 'POST'){
-            return path;
-        }else{
-            return '/1/'+ path;
-        }
-    })
-</function>
+window.$HandleBars.registerHelper('checkExtendVerbType', function (type, path) {
+    if(type == 'POST'){
+        return path;
+    }else{
+        return '/1/'+ path;
+    }
+})
