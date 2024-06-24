5f2fe98b79e02db288634120bcd37947af1de24a
kyusoo <rbtn110@uengine.org>
2024-06-24 15:31:41 +0900

    pom.xml 수정

--- src/test/java/com/example/template/AggregateRestBase.java.java
+++ src/test/java/com/example/template/AggregateRestBase.java.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import {{options.package}}.infra.{{namePascalCase}}Controller;
 import {{options.package}}.{{namePascalCase}}Application;
