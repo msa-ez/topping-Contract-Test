723b09f99fba7761f0dfc82dc6911426da1e545f
kyusoo <rbtn110@uengine.org>
2024-06-24 16:59:22 +0900

    url 수정

--- AggregateGet.groovy
+++ AggregateGet.groovy
@@ -7,7 +7,7 @@ package contracts.rest
 org.springframework.cloud.contract.spec.Contract.make {
     request {
         method '{{#attached "Event" this}}{{#outgoingRelations}}{{#target}}{{#if aggregate}}{{controllerInfo.method}}{{/if}}{{/target}}{{/outgoingRelations}}{{/attached}}'
-        url ('/{{#attached "Event" this}}{{#outgoingRelations}}{{#target}}{{#if aggregate}}{{aggregate.namePlural}}{{#checkExtendVerbType controller.method controllerInfo.apiPath}}{{/checkExtendVerbType}}{{/if}}{{/target}}{{/outgoingRelations}}{{/attached}}')
+        url ('/{{#attached "Event" this}}{{#outgoingRelations}}{{#target}}{{#if aggregate}}{{aggregate.namePlural}}/1{{#checkExtendVerbType controller.method controllerInfo.apiPath}}{{/checkExtendVerbType}}{{/if}}{{/target}}{{/outgoingRelations}}{{/attached}}')
         headers {
             contentType(applicationJsonUtf8())
         }
