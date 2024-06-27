ddcd149959947d3e519821b6e0a19be74454c7e5
kyusoo <rbtn110@uengine.org>
2024-06-27 10:43:38 +0900

    코드 수정

--- src/test/java/com/example/template/MessagingBase.java
+++ src/test/java/com/example/template/MessagingBase.java
@@ -43,13 +43,13 @@ public abstract class MessagingBase {
         {{#aggregate}}
         {{namePascalCase}} {{nameCamelCase}} = new {{namePascalCase}}();
 
-        {{#aggregateRoot.fieldDescriptors}}
-        {{#../../../examples}}
+        {{#../../examples}}
         {{#when}}
-        {{../../../nameCamelCase}}.set{{../../namePascalCase}}({{#checkExampleType value ../../../../../incomingRelations}}{{/checkExampleType}});
+        {{#each value}}
+        {{#setAggExample @key this ../../../incomingRelations}}{{/setAggExample}};
+        {{/each}}
         {{/when}}
-        {{/../../../examples}}
-        {{/aggregateRoot.fieldDescriptors}}
+        {{/../../examples}}
 
         {{../namePascalCase}} {{../nameCamelCase}} = new {{../namePascalCase}}({{nameCamelCase}});
         // orderPlaced.setEventType("OrderPlaced");
@@ -68,56 +68,47 @@ public abstract class MessagingBase {
     window.$HandleBars.registerHelper('checkExample', function (examples) {
         if(examples) return false;
     })
-    window.$HandleBars.registerHelper('checkExampleType', function (value, incoming) {
+    window.$HandleBars.registerHelper('checkExampleType', function (key, value, incoming) {
         var type = 'String';
         var quote = "'";
-        var result = {};
-    
-        Object.keys(value).forEach(function(key) {
-            for(var i = 0; i < incoming.length; i++){
-                for(var j = 0; j < incoming[i].source.aggregate.aggregateRoot.fieldDescriptors.length; j++){
-                    if(incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].name == key){
-                        type = incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].className;
-                        break;
-                    }
+        var aggName = null;
+        for(var i = 0; i < incoming.length; i++){
+            aggName = incoming[i].source.aggregate.nameCamelCase; + ' set' + key.substring(0, 1).toUpperCase() + key.substring(1)
+            for(var j = 0; j< incoming[i].source.aggregate.aggregateRoot.fieldDescriptors.length; j++){
+                if(incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].name == key){
+                    type = incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].className
                 }
             }
-    
-            switch (type) {
-                case 'String':
-                    result[key] = quote + value[key] + quote; // Java에서 문자열은 큰따옴표를 사용합니다.
-                    break;
-                case 'Long':
-                    result[key] = `${value[key]}L`; // long 타입으로 간주할 수 있습니다.
-                    break;
-                case 'Integer':
-                    result[key] = `${value[key]}`;
-                    break;
-                case 'Boolean':
-                    result[key] = value[key].toString();
-                    break;
-                case 'Object':
-                    if (value[key] instanceof Date) {
-                        result[key] = `new Date(${value[key].getTime()}L)`; // Java의 Date 생성자를 사용합니다.
-                    } else if (value[key] === null) {
-                        result[key] = 'null';
-                    } else if (Array.isArray(value[key])) {
-                        const elements = value[key].map((element) => convertToJavaSyntax(element)).join(', ');
-                        result[key] = `new Object[]{${elements}}`; // Object 배열로 간주합니다.
-                    } else if (typeof value[key] === 'object') {
-                        // 객체를 JSON 문자열로 변환
-                        result[key] = JSON.stringify(value[key]);
-                    } else {
-                        result[key] = value[key].toString(); // 기본적인 toString 반환을 사용합니다.
-                    }
-                    break;
-                default:
-                    throw new Error(`Unsupported type: ${type}`);
+        }
+        
+        switch (type) {
+            case 'String':
+            return aggName +'('quote + value + quote ')'; // Java에서 문자열은 큰따옴표를 사용합니다.
+            case 'Long':
+            // JavaScript의 숫자는 정수 또는 부동소수점일 수 있으므로 이를 구분해야 할 수도 있습니다.
+                return aggName + '(' + `${value}L` + ')'; // long 타입으로 간주할 수 있습니다.
+            case 'Integer':
+                return aggName + '(' + `${value}` + ')'; 
+            case 'Boolean':
+            return aggName + '(' + value.toString() + ')';
+            case 'Object':
+            if (value instanceof Date) {
+                return `new Date(${value.getTime()}L)`; // Java의 Date 생성자를 사용합니다.
+            } else if (value === null) {
+                return aggName + '(' + 'null' ')';
+            } else if (Array.isArray(value)) {
+                // 배열의 경우 더 복잡한 로직이 필요할 수 있으며, 이는 예시로만 제공됩니다.
+                const elements = value.map((element) => convertToJavaSyntax(element)).join(', ');
+                return `new Object[]{${elements}}`; // Object 배열로 간주합니다.
+            } else {
+                // 다른 종류의 객체에 대한 처리가 필요할 수 있습니다.
+                // 이 경우 해당 객체를 적절한 Java 표현으로 변환하는 로직이 필요합니다.
+                return value.toString(); // 기본적인 toString 반환을 사용합니다.
             }
-        });
-    
-        return result;
-    });
+            default:
+            throw new Error(`Unsupported type: ${type}`);
+        }
+    })
     window.$HandleBars.registerHelper('setExampleType', function (key, value, incoming) {
         var type = 'String'
         for(var i = 0; i < incoming.length; i++){
