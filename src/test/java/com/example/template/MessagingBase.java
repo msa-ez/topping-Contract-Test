d50e751c87604043131c3700d34ddfb4be5845ab
kyusoo <rbtn110@uengine.org>
2024-06-27 10:53:40 +0900

    코드 수정

--- src/test/java/com/example/template/MessagingBase.java
+++ src/test/java/com/example/template/MessagingBase.java
@@ -69,33 +69,32 @@ public abstract class MessagingBase {
         if(examples) return false;
     })
     window.$HandleBars.registerHelper('checkExampleType', function (key, value, incoming) {
-        var type = 'String';
+        var examplesType = 'String';
         var quote = "'";
         var aggName = null;
         for(var i = 0; i < incoming.length; i++){
             aggName = incoming[i].source.aggregate.nameCamelCase; + ' set' + key.substring(0, 1).toUpperCase() + key.substring(1)
             for(var j = 0; j< incoming[i].source.aggregate.aggregateRoot.fieldDescriptors.length; j++){
                 if(incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].name == key){
-                    type = incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].className
+                    examplesType = incoming[i].source.aggregate.aggregateRoot.fieldDescriptors[j].className
                 }
             }
         }
         
-        switch (type) {
+        switch (examplesType) {
             case 'String':
-            return aggName +'(' + quote + value + quote + ')'; // Java에서 문자열은 큰따옴표를 사용합니다.
+            return aggName + '(' + quote + value + quote + ')'; 
             case 'Long':
-            // JavaScript의 숫자는 정수 또는 부동소수점일 수 있으므로 이를 구분해야 할 수도 있습니다.
-                return aggName + '(' + `${value}L` + ')'; // long 타입으로 간주할 수 있습니다.
+                return aggName + '(' + `${value}L` + ')'; 
             case 'Integer':
                 return aggName + '(' + `${value}` + ')'; 
             case 'Boolean':
             return aggName + '(' + value.toString() + ')';
             case 'Object':
             if (value instanceof Date) {
-                return `new Date(${value.getTime()}L)`; // Java의 Date 생성자를 사용합니다.
+                return `new Date(${value.getTime()}L)`; 
             } else if (value === null) {
-                return aggName + '(' + 'null' ')';
+                return aggName + '(' + 'null' + ')';
             } else if (Array.isArray(value)) {
                 // 배열의 경우 더 복잡한 로직이 필요할 수 있으며, 이는 예시로만 제공됩니다.
                 const elements = value.map((element) => convertToJavaSyntax(element)).join(', ');
@@ -106,7 +105,7 @@ public abstract class MessagingBase {
                 return value.toString(); // 기본적인 toString 반환을 사용합니다.
             }
             default:
-            throw new Error(`Unsupported type: ${type}`);
+            throw new Error(`Unsupported type: ${examplesType}`);
         }
     })
     window.$HandleBars.registerHelper('setExampleType', function (key, value, incoming) {
