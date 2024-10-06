0e4ae583edbb523e2335334b9d33ff49383307ac
kyusoo <rbtn110@uengine.org>
2024-10-06 15:53:36 +0900

    이름 수정

--- src/test/java/com/example/template/PolicyContractTest.java
+++ src/test/java/com/example/template/PolicyContractTest.java
@@ -5,7 +5,7 @@ except: {{#checkExample examples}}{{/checkExample}}
 ---
 package com.example.template;
 
-import {{options.package}}.{{{boundedContext.namePascalCase}}Application;
+import {{options.package}}.{{#boundedContext}}{{namePascalCase}}{{/boundedContext}}Application;
 import {{options.package}}.config.kafka.KafkaProcessor;
 
 import com.jayway.jsonpath.DocumentContext;
